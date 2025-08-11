0ff829326532c61303a2ae3f5638224b783067e6
ronit873 <ronitmishra873@gmail.com>
2025-08-11 21:28:03 +0530

    Add files via upload

---
+++ reverse a dll.py
@@ -0,0 +1,7 @@
+class Solution:
+    
+    def reverseDLL(self, head):
+        while head:
+            head.next, head.prev = head.prev, head.next
+            if not head.prev:return head
+            head=head.prev
